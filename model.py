4e697383a59b2e41b2c53819122c340d4a5e19e5
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-21 23:14:09 +0530

    Model modified

--- model.py
+++ model.py
@@ -13,12 +13,8 @@ from tensorflow.keras.layers import GlobalAveragePooling1D, GlobalMaxPooling1D
 def get_model(X_train):
     inp = Input(shape=(X_train.shape[1],X_train.shape[2]))
     x = LSTM(512, activation = 'tanh', recurrent_activation = 'sigmoid', recurrent_dropout = 0, unroll = False, use_bias = True, return_sequences = True)(inp)
-    x = Dropout(0.25)(x)
     x = LSTM(256, activation = 'tanh', recurrent_activation = 'sigmoid', recurrent_dropout = 0, unroll = False, use_bias = True, return_sequences = True)(inp)
-    x = Dropout(0.25)(x)
-    max_pool = GlobalMaxPooling1D()(x)
-    avg_pool = GlobalAveragePooling1D()(x)
-    x = concatenate([max_pool,avg_pool])
+    x = GlobalMaxPooling1D()(x)
     x = Dense(1024)(x)
     x = Dropout(0.25)(x)
     preds = Dense(X_train.shape[2])(x)
